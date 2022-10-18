dbe0e5ce5acc2ef2f63fc507002482c9d5651fb6
Max Boiko <maxboiko7@icloud.com>
2022-10-18 12:09:09 +0300

    test

--- js/firstScript.js
+++ js/firstScript.js
@@ -1,2 +1,8 @@
-const yearOfBirth = 2006;
-console.log(yearOfBirth);
+let password = "";
+
+do {
+  password = prompt("Введите пароль длиннее 4-х символов", "");
+} while (password.length < 5);
+
+console.log("Ввели пароль: ", password);
+console.log (password)
